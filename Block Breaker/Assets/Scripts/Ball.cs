197f04562785effa23c3cc64f5fd521a560dd47d
Maxi Lencina <lencinamaxiezequiel@gmail.com>
2019-10-07 21:46:57 -0300

    ball loop solution

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -4,25 +4,18 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 
     [SerializeField] Paddle paddle;
-    
     Vector2 distancePaddleBall;
-    
     bool hasStarted = false;
     //Audio Files
-    
     [SerializeField] AudioClip[] ballSounds;
-    
     AudioSource ballAudioSource;
-    
     [SerializeField] float randomFactor = 1f;
-
     Vector2 lastBouncePosition;
-
     Rigidbody2D ballRigidBody;
-
     Vector3 force;
-
     Vector2 screenLimits;
+    short xLoopCounter;
+    short yLoopCounter;
 
     // Use this for initialization
     void Start() {
@@ -33,7 +26,7 @@ public class Ball : MonoBehaviour {
         ballAudioSource = GetComponent<AudioSource>();
 
         ballRigidBody = GetComponent<Rigidbody2D>();
-        force = new Vector3(0.5f , 0.5f, 0);
+        force = new Vector3(1f , 1f, 0);
 
         Vector3 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,0));
         screenLimits = new Vector2(0,0);
@@ -61,9 +54,7 @@ public class Ball : MonoBehaviour {
         transform.position = paddlePos + distancePaddleBall;
     }
 
-    private bool IsBallInLoop(){
-        return IsBallInXAxisLoop() || IsBallInYAxisLoop();
-    }
+    
 
     private bool IsBallInYAxisLoop(){
         Vector2 currentPosition = transform.position;
@@ -82,17 +73,42 @@ public class Ball : MonoBehaviour {
             //playOneShot is used to play a sound and to not be interrupted by other ones
             ballAudioSource.PlayOneShot(audioClip);
             
-            if(IsBallInLoop())
-               AddForceToBall();
+            if(IsBallInXAxisLoop()){
+                if(xLoopCounter >= 3)
+                    AddForceToBall(true);
+                else 
+                    xLoopCounter++;
+            }
+            else if(IsBallInYAxisLoop()){
+                if(yLoopCounter >= 3)
+                    AddForceToBall(false);
+                else
+                    yLoopCounter++;
+            }
             lastBouncePosition = transform.position;
         }
     }
 
-    private void AddForceToBall(){
-        if( (int)Math.Truncate(transform.position.x) == (int)Math.Truncate(screenLimits.x) || (int)Math.Truncate(transform.position.x) == 0 )
-            force.x *= -1;
-        if( (int)Math.Truncate(transform.position.y) == (int)Math.Truncate(screenLimits.y) || (int)Math.Truncate(transform.position.y) == 0 )
-            force.y *= -1;
-        ballRigidBody.AddForce(force, ForceMode2D.Impulse);
+    private void AddForceToBall(bool isXLoop){
+        /*Add force to the gameobject so it can move out of the loop bouncing and remove it to restore the object's 
+        initial velocity */
+        if(isXLoop){
+            bool isBallAtScreenWitdhLimit = (int)Math.Truncate(transform.position.x) == (int)Math.Truncate(screenLimits.x);
+            bool isBallAtScreenWitdhStart = (int)Math.Truncate(transform.position.x) == 0;
+            if( isBallAtScreenWitdhLimit || isBallAtScreenWitdhStart )
+                force.x *= -1;
+            xLoopCounter = 0;
+            ballRigidBody.AddForce(new Vector3(force.x, 0, 0), ForceMode2D.Impulse);
+        }
+        else{
+            bool isBallAtScreenHeightLimit = (int)Math.Truncate(transform.position.y) == (int)Math.Truncate(screenLimits.y);
+            bool isBallAtScreenHeightStart = (int)Math.Truncate(transform.position.y) == 0;
+            if( isBallAtScreenHeightLimit || isBallAtScreenHeightStart )
+                force.y *= -1;
+            yLoopCounter = 0;
+            ballRigidBody.AddForce(new Vector3(0, force.y, 0), ForceMode2D.Impulse);
+        }
+        
+        
     }
 }
